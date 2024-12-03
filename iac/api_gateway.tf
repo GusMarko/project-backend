54322ca481ba353fbd51d377f39a5fb31e136e57
Marko Gusic <markoggusic@gmail.com>
2024-12-03 23:11:10 +0100

    fix api method mistake

--- iac/api_gateway.tf
+++ iac/api_gateway.tf
@@ -14,7 +14,7 @@ resource "aws_api_gateway_resource" "search" {
 resource "aws_api_gateway_method" "get" {
   rest_api_id   = aws_api_gateway_rest_api.spotify_api.id
   resource_id   = aws_api_gateway_resource.search.id
-  http_method   = "POST"
+  http_method   = "GET"
   authorization = "NONE"  
 }
 
