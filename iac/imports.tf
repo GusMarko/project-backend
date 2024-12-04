9a4dc2ae07d452aad0be3eadde129046d7ef0a9d
Marko Gusic <markoggusic@gmail.com>
2024-12-04 08:21:26 +0100

    remove configuration in imports.tf

--- iac/imports.tf
+++ iac/imports.tf
@@ -2,7 +2,6 @@ data "terraform_remote_state" "networking" {
   backend = "s3"
 
   config = {
-    organization = "hashicorp"
     bucket = "mg-terraform-state-storage"
     key = "project-networking/terraform.tfstate"
     region = "${var.aws_region}"
