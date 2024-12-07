4918923194882d4b9d3bd9f4cd3b2429c4c76d85
Marko Gusic <markoggusic@gmail.com>
2024-12-07 23:12:43 +0100

    change integration get to post

--- iac/api_gateway.tf
+++ iac/api_gateway.tf
@@ -25,7 +25,7 @@ resource "aws_api_gateway_integration" "lambda_integration" {
   rest_api_id             = aws_api_gateway_rest_api.spotify_api.id
   resource_id             = aws_api_gateway_resource.search.id
   http_method             = aws_api_gateway_method.get.http_method
-  integration_http_method = "GET"
+  integration_http_method = "POST"
   type                    = "AWS_PROXY"
   uri                     = aws_lambda_function.lambda.invoke_arn
 }
