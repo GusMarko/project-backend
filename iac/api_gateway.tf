a903049af6ec66ef0dd1540177d376a99d030f66
Marko Gusic <markoggusic@gmail.com>
2024-12-08 07:56:12 +0100

    change integration get to get

--- iac/api_gateway.tf
+++ iac/api_gateway.tf
@@ -37,7 +37,7 @@ resource "aws_api_gateway_integration" "options_integration" {
   }
 }
 
-resource "aws_api_gateway_method_response" "options_response" {
+resource "aws_api_gateway_integration_response" "options_response" {
   rest_api_id = aws_api_gateway_rest_api.spotify_api.id
   resource_id = aws_api_gateway_resource.search.id
   http_method = aws_api_gateway_method.options.http_method
