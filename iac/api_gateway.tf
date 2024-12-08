31743a488624d1a444fb2bee44e22729592ae6c1
Marko Gusic <markoggusic@gmail.com>
2024-12-08 13:55:42 +0100

    bring back old commit with post integration http method

--- iac/api_gateway.tf
+++ iac/api_gateway.tf
@@ -28,8 +28,8 @@ resource "aws_api_gateway_method" "options" {
 resource "aws_api_gateway_integration" "get_integration" {
   rest_api_id = aws_api_gateway_rest_api.spotify_api.id
   resource_id = aws_api_gateway_resource.search.id
-  integration_http_method = "GET"
   http_method = aws_api_gateway_method.get.http_method
+  integration_http_method = "POST"
   type        = "AWS_PROXY"
   uri = aws_lambda_function.lambda.invoke_arn
   
@@ -55,7 +55,7 @@ resource "aws_api_gateway_integration_response" "options_response" {
 
   response_parameters = {
     "method.response.header.Access-Control-Allow-Headers" = "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token,X-Amz-User-Agent'"
-    "method.response.header.Access-Control-Allow-Methods" = "'GET,OPTIONS'"
+    "method.response.header.Access-Control-Allow-Methods" = "'GET,OPTIONS,POST'"
     "method.response.header.Access-Control-Allow-Origin"  = "'*'"
   }
 }
