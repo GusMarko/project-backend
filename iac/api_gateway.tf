4a0f1bd1a51bb44ab2473af10a43aff2c9dd85ca
Marko Gusic <markoggusic@gmail.com>
2024-12-08 08:03:47 +0100

    change integration get to get

--- iac/api_gateway.tf
+++ iac/api_gateway.tf
@@ -28,7 +28,7 @@ resource "aws_api_gateway_method" "options" {
 resource "aws_api_gateway_integration" "get_integration" {
   rest_api_id = aws_api_gateway_rest_api.spotify_api.id
   resource_id = aws_api_gateway_resource.search.id
-   http_method = "GET"
+   http_method = aws_api_gateway_method.get.http_method
    type        = "AWS_PROXY"
   
 }
