234b6007808b3d7a995fee5168f76666546b6d0a
Marko Gusic <markoggusic@gmail.com>
2024-12-10 10:55:01 +0100

    add bigger lambda timeout

--- iac/lambda.tf
+++ iac/lambda.tf
@@ -40,7 +40,7 @@ resource "aws_lambda_function" "lambda" {
   package_type  = "Image"
   image_uri     = var.image_uri
   role          = aws_iam_role.lambda_role.arn
-  timeout       = 20
+  timeout       = 120
   memory_size   = 128
 
   environment {
